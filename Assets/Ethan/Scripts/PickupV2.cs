f006e47cf8868aea61d908bcb2294152bc8d6458
ehsmith1153 <ehsmith1153@gmail.com>
2022-01-29 13:38:52 -0500

    Fixed pick up and drop system.
    Added "moveable" tag to denote items that can be picked up.

--- Assets/Ethan/Scripts/PickupV2.cs
+++ Assets/Ethan/Scripts/PickupV2.cs
@@ -7,6 +7,8 @@ using UnityEngine.InputSystem;
 public class PickupV2 : MonoBehaviour
 {
     private GameObject heldObj;
+
+    public GameObject player;
     public float pickUpRange = 5;
     public Transform holdParent;
     public float moveForce = 20;
@@ -41,12 +43,14 @@ public class PickupV2 : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
               {
-                Rigidbody pickupObjRig = hit.transform.gameObject.GetComponent<Rigidbody>();
-                pickupObjRig.transform.position = holdParent.transform.position;
-                pickupObjRig.useGravity = false;
-                pickupObjRig.drag = 10;
-                pickupObjRig.transform.parent = holdParent;
-                heldObj = pickupObjRig.transform.gameObject;
+                if (hit.transform.gameObject.CompareTag("moveable")) {
+                    Rigidbody pickupObjRig = hit.transform.gameObject.GetComponent<Rigidbody>();
+                    pickupObjRig.transform.position = holdParent.transform.position;
+                    pickupObjRig.useGravity = false;
+                    pickupObjRig.drag = 10;
+                    pickupObjRig.transform.parent = holdParent;
+                    heldObj = pickupObjRig.transform.gameObject;
+                }
               }
         }
         else
@@ -57,6 +61,25 @@ public class PickupV2 : MonoBehaviour
 
             heldObj.transform.parent = null;
             heldObj = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (heldObj != null)
+        {
+            heldObj.transform.position = holdParent.transform.position;
+            heldObj.transform.rotation = player.transform.rotation;
+            //moveObject();
+        }
+    }
+
+    private void moveObject()
+    {
+        if (Vector3.Distance(heldObj.transform.position, holdParent.position) > 0.1f)
+        {
+            Vector3 moveDirection = (holdParent.position = heldObj.transform.position);
+            heldObj.GetComponent<Rigidbody>().AddForce(moveDirection * moveForce);
         }
     }
 }
